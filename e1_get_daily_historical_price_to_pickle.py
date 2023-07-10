11719ff97dcc5d6bcb77dcc09b8fbdcb509ec270
unknown <kangeun@naver.com>
2023-07-10 18:20:33 +0900

    get daily company data

--- e1_get_daily_historical_price_to_pickle.py
+++ e1_get_daily_historical_price_to_pickle.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 from selenium import webdriver as wd
 from bs4 import BeautifulSoup as bs
 import requests
@@ -11,6 +8,11 @@ import pandas as pd
 import re
 import os, sys
 
+import cfscrape  # Forbidden 403 발생시 requests 대신 사용
+scraper = cfscrape.create_scraper()
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 # 삼성전자 005930, 현대차 005380, 네이버 035420, KT&G 033780, 
 code = {'sec': 'KR7005930003', 'hyunmotor': 'KR7005380001', 'naver': 'KR7035420009', 'ktng': 'KR7033780008'}
@@ -23,8 +25,10 @@ def get_data(ticker, today):
     url_base = 'https://stock.mk.co.kr/price/hourly?stock_code='
     for i in range(8):
         str1 = str(i+1)
-        url = '{}{}{}{}{}{}'.format(url_base, ticker, '&day=', today, '&page=', str1)
-        df1 = pd.read_html(url, attrs={"class": "table-stock line"}, flavor=["lxml", "bs4"])[0]
+        url = '{}{}{}{}{}{}'.format(url_base, ticker, '&day=', today, '&page=', str1)    
+        r = scraper.get(url, headers=headers)  
+        html = r.content
+        df1 = pd.read_html(html, attrs={"class": "table-stock line"}, flavor=["lxml", "bs4"])[0]
         if i == 0 :
             df = df1
         else:
@@ -48,7 +52,3 @@ for key, val in code.items():
     df.to_pickle(filename)
     filename = '{}{}{}{}.csv'.format(dir_name, key, f_name, today) # code만 기입
     df.to_csv(filename)
-
-
-
-
